8e6f57dc6100d7a395f393a4cead537c97b60f31
DarthAV <Ansh.Verma22@outlook.com>
2020-07-13 16:33:15 -0700

    Built InPlaceArrayMerge

---
+++ Algorithims/InPlaceArrayMerge.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithims_Practice
+{
+    public class InPlaceArrayMerge
+    {
+        public static void InPlaceMerge(int[] smallArray, int[] bigArray)
+        {
+            if (smallArray == null || smallArray.Length != 0 || bigArray == null || bigArray.Length != 0)
+            {
+                merge(smallArray, bigArray);
+            }
+        }
+
+        private static void merge(int[] smallArray, int[] bigArray)
+        {
+            int smallArrayPointer = smallArray.Length - 1;
+            int bigArrayPointer = bigArray.Length - smallArray.Length - 1;
+            int constuctionPointer = bigArray.Length - 1;
+
+            for (int i = bigArray.Length - 1; i >= 0; i--)
+            {
+                if (smallArrayPointer >= 0 && bigArrayPointer >= 0)
+                {
+                    if (bigArray[bigArrayPointer] >= smallArray[smallArrayPointer])
+                    {
+                        bigArray[i] = bigArray[bigArrayPointer--];
+                    }
+                    else
+                    {
+                        bigArray[i] = smallArray[smallArrayPointer--];
+                    }
+                }
+                else
+                {
+                    if (smallArrayPointer >= 0)
+                    {
+                        bigArray[i] = smallArray[smallArrayPointer--];
+                    }
+                    else if (bigArrayPointer >= 0)
+                    {
+                        bigArray[i] = bigArray[bigArrayPointer--];
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnitTests/InPlaceArrayMergeTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Algorithims_Practice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class InPlaceArrayMergeTests
+    {
+        [TestMethod]
+        public void InPlaceArrayMergeTest()
+        {
+            var smallArray = new int[] { 1, 9, 10, 15 };
+            var bigArray = new int[] { 2, 5, 11, 17, 0, 0, 0, 0 };
+            var expected = new int[] { 1, 2, 5, 9, 10, 11, 15, 17 };
+            InPlaceArrayMerge.InPlaceMerge(smallArray, bigArray);
+            TestUtility.AssertArraysAreEqual(expected, bigArray);
+        }
+
+        [TestMethod]
+        public void InPlaceArrayMergeTestWithZero()
+        {
+            var smallArray = new int[] { 0, 9, 10, 15 };
+            var bigArray = new int[] { 0, 5, 11, 17, 0, 0, 0, 0 };
+            var expected = new int[] { 0, 0, 5, 9, 10, 11, 15, 17 };
+            InPlaceArrayMerge.InPlaceMerge(smallArray, bigArray);
+            TestUtility.AssertArraysAreEqual(expected, bigArray);
+        }
+
+
+
+        [TestMethod]
+        public void InPlaceArrayMergeTestEmpty()
+        {
+            var smallArray = new int[] { };
+            var bigArray = new int[] { };
+            var expected = new int[] { };
+            InPlaceArrayMerge.InPlaceMerge(smallArray, bigArray);
+            TestUtility.AssertArraysAreEqual(expected, bigArray);
+        }
+    }
+}
