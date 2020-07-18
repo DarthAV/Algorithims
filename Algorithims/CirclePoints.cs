9f7420b0f6f3daf9ee44c6fb8ff9ea245bd69a2d
DarthAV <Ansh.Verma22@outlook.com>
2020-07-17 17:22:55 -0700

    made program that checks if point is inside circle

---
+++ Algorithims/CirclePoints.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithims_Practice
+{
+    public class CirclePoints
+    {
+
+        public static bool checkPointInside(Circle circle, int x, int y)
+        {
+            var distance = getDistanceFromCenter(circle, x, y);
+            if (circle.radius >= distance)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static double getDistanceFromCenter(Circle circle, int x, int y)
+        {
+            var difX = Math.Abs(circle.centerX - x);
+            var difY = Math.Abs(circle.centerY - y);
+            var dist = Math.Sqrt(Math.Pow(difX, 2) + Math.Pow(difY, 2));
+            return dist;
+        }
+    }
+    public class Circle
+    {
+        internal int centerX { get; set; }
+        internal int centerY { get; set; }
+        internal int radius { get; set; }
+
+        public Circle(int centerX, int centerY, int radius)
+        {
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.radius = radius;
+
+        }
+
+
+    }
+
+}
---
+++ UnitTests/CirclePointsTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithims_Practice;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CirclePointsTest
+    {
+        [TestMethod]
+        public void PointInsideCircle()
+        {
+            var circle = new Circle(0, 0, 5);
+            var pointX = 3;
+            var pointY = 2;
+            var expected = true;
+            var actual = CirclePoints.checkPointInside(circle, pointX, pointY);
+            Assert.IsTrue(expected == actual);
+        }
+
+        [TestMethod]
+        public void PointOutsideCircle()
+        {
+            var circle = new Circle(0, 0, 5);
+            var pointX = 5;
+            var pointY = 7;
+            var expected = false;
+            var actual = CirclePoints.checkPointInside(circle, pointX, pointY);
+            Assert.IsTrue(expected == actual);
+        }
+
+        [TestMethod]
+        public void PointOnCircle()
+        {
+            var circle = new Circle(0, 0, 5);
+            var pointX = 5;
+            var pointY = 0;
+            var expected = true;
+            var actual = CirclePoints.checkPointInside(circle, pointX, pointY);
+            Assert.IsTrue(expected == actual);
+        }
+
+        [TestMethod]
+        public void PointInsideCircleQuadrant2()
+        {
+            var circle = new Circle(0, 0, 5);
+            var pointX = -4;
+            var pointY = -1;
+            var expected = true;
+            var actual = CirclePoints.checkPointInside(circle, pointX, pointY);
+            Assert.IsTrue(expected == actual);
+        }
+        [TestMethod]
+        public void PointOutsideCircleQuadrant2()
+        {
+            var circle = new Circle(0, 0, 5);
+            var pointX = -5;
+            var pointY = -2;
+            var expected = false;
+            var actual = CirclePoints.checkPointInside(circle, pointX, pointY);
+            Assert.IsTrue(expected == actual);
+        }
+    }
+}
